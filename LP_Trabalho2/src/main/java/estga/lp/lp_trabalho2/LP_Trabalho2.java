f02d288a67dc2e9504877f3dea556285d16aec50
SebastianMazoSimoes <sebastianmsimoes@ua.pt>
2026-03-19 16:46:18 +0000

    1º commit "Teste"

---
+++ LP_Trabalho2/src/main/java/estga/lp/lp_trabalho2/LP_Trabalho2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package estga.lp.lp_trabalho2;
+
+/**
+ *
+ * @author SebastianMazoSimoes
+ */
+public class LP_Trabalho2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
